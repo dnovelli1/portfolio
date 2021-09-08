72dc56a21bee0281dd27c01451e83c51eae99d65
Jake Novelli <jakenovelli11@gmail.com>
2021-09-08 15:50:26 -0700

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Navbar />
   );
 }
 
