1cfe0567e37444d8f531939bc8993e61554bd6e7
Jake Novelli <jakenovelli11@gmail.com>
2021-09-08 17:09:41 -0700

    added background image and installed typed text pckg

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="headerWrap">
+            <div className="quickInfo">
+                <h1>Front End Developer</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
 
 const Navbar = () => {
     return (
@@ -6,7 +9,7 @@ const Navbar = () => {
             <div className="container">
                 <a className="navbar-brand" href="#">Dev by Jake</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto" style={{ marginLeft: 'auto' }}>
