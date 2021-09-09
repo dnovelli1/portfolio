9a5dff584242032e550f41be6594482ba2c3eeb9
Jake Novelli <jakenovelli11@gmail.com>
2021-09-09 15:54:30 -0700

    added typed text with buttons for links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Types from "react-typed";
 
 const Header = () => {
     return (
         <div className="headerWrap">
             <div className="quickInfo">
-                <h1>Front End Developer</h1>
+                <Types 
+                className="typed-text"
+                strings={["Web Developer", "Software Engineer", ""]}
+                typeSpeed={30}
+                backSpeed={60}
+                loop
+                />
+                <a href="https://github.com/dnovelli1" className="btn-home">Github</a>
+                <a href="https://www.linkedin.com/in/david-jacob-novelli/" className="btn-home">LinkedIn</a>
             </div>
         </div>
     )
