f9d457c1df49254d0c987456d56134b010ff2e09
Jake Novelli <jakenovelli11@gmail.com>
2021-10-06 15:51:42 -0700

    added cards for projects and refactored projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,36 +1,50 @@
-import React from "react"
+import React from "react";
 
 const Projects = () => {
-    var project = [
-        {
-            descrip: "",
-            technology: "",
-            github: "",
-            link: ""
-        },
-        {
-            descrip: "",
-            technology: "",
-            github: "",
-            link: ""
-        },
-        {
-            descrip: "",
-            technology: "",
-            github: "",
-            link: ""
-        },
-        {
-            descrip: "",
-            technology: "",
-            github: "",
-            link: ""
-        }
-    ];
-
     return (
-        <div>
-            <h1>This is my projects section</h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="card text-center">
+                        <img src="..." className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card text-center">
+                        <img src="..." className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card text-center">
+                        <img src="..." className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card text-center">
+                        <img src="..." className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">Special title treatment</h5>
+                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
