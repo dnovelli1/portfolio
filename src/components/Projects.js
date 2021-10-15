cf50c12f5ec747660a2dc23af74112f019f21653
Jake Novelli <jakenovelli11@gmail.com>
2021-10-15 15:42:30 -0700

    added project images and links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+import crypto from "../assets/cryptonite.gif";
 
 const Projects = () => {
     return (
@@ -6,11 +7,23 @@ const Projects = () => {
             <div className="row">
                 <div className="col-sm-6">
                     <div className="card text-center">
+                        <img src={crypto} className="card-img-top" />
+                        <div className="card-body">
+                            <h5 className="card-title">Cryptonite</h5>
+                            <p className="card-text">A cryptocurrency full-stack application built with React.js. Create an account, buy/send coins, or check on the current price of Cryptonite!</p>
+                            <a href="https://github.com/dnovelli1/Crytonite-a-cryptocurrency-with-MERN" className="btn btn-primary">Github</a>
+                            <a href="http://cryptonite-env.eba-kvnfavpx.us-west-2.elasticbeanstalk.com/" className="btn btn-primary">Full Site</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <div className="card text-center">
                         <img src="..." className="card-img-top" />
                         <div className="card-body">
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <h5 className="card-title">Sporcle But Better</h5>
+                            <p className="card-text">Test your knowledge on the capitals of countries in each continent. Compare your scores to others and track your progress.</p>
+                            <a href="https://github.com/dnovelli1/sporclebutbetter" className="btn btn-primary">Github</a>
+                            <a href="https://radiant-chamber-32939.herokuapp.com/" className="btn btn-primary">Full Site</a>
                         </div>
                     </div>
                 </div>
@@ -20,7 +33,8 @@ const Projects = () => {
                         <div className="card-body">
                             <h5 className="card-title">Special title treatment</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <a href="https://github.com/dnovelli1/Crytonite-a-cryptocurrency-with-MERN" className="btn btn-primary">Github</a>
+                            <a href="http://cryptonite-env.eba-kvnfavpx.us-west-2.elasticbeanstalk.com/" className="btn btn-primary">Full Site</a>
                         </div>
                     </div>
                 </div>
@@ -30,17 +44,8 @@ const Projects = () => {
                         <div className="card-body">
                             <h5 className="card-title">Special title treatment</h5>
                             <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-sm-6">
-                    <div className="card text-center">
-                        <img src="..." className="card-img-top" />
-                        <div className="card-body">
-                            <h5 className="card-title">Special title treatment</h5>
-                            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                            <a href="#" className="btn btn-primary">Go somewhere</a>
+                            <a href="https://github.com/dnovelli1/Crytonite-a-cryptocurrency-with-MERN" className="btn btn-primary">Github</a>
+                            <a href="http://cryptonite-env.eba-kvnfavpx.us-west-2.elasticbeanstalk.com/" className="btn btn-primary">Full Site</a>
                         </div>
                     </div>
                 </div>
